3960c6d0991ac5e33c4b98b082b41c430238cc5b
DhivakharVenkatachalam <dhivakharjv@gmail.com>
2021-06-03 21:41:31 +0530

    Change code to ES6 standards

--- stub_handler.js
+++ stub_handler.js
@@ -3,8 +3,8 @@ import * as fs from 'fs'
 import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path'
 import {Lists3objects,getHostAndKeyFromUrl,getS3Objects,putS3Objects,ValidateUrlForS3} from './lib/s3botoutils.js'
-// import {extractFile,ZipFile} from './lib/fileutils.js'
-// import {extractStubs} from './lib/stubscriptutils.js'
+import {extractFile,ZipFile} from './lib/fileutils.js'
+import {extractStubs} from './lib/stubscriptutils.js'
 
 const tmp = os.tmpdir()
 const unique_id = uuidv4()
@@ -23,23 +23,23 @@ const a = async (event) => {
 
         var file_objects = await Lists3objects(proto.host, proto.path)
         
-        for (var i = 0; i < 1; i++) {
+        for (var i = 0; i < file_objects.length; i++) {
             const Isvalid = ValidateUrlForS3(file_objects[i])
             if (path.extname(file_objects[i]) == '.zip' && Isvalid) {
 
                 await getS3Objects(proto.host, file_objects[i], download_file)
-                // await extractFile(download_file, extracted_file)
+                await extractFile(download_file, extracted_file)
 
-                // const extracted = await fs.promises.opendir(extracted_file)
-                // for await (const dirent of extracted) {
-                //     console.log(`Generating stub for ${dirent.name}`)
-                //     await extractStubs(dirent.name, `${unique_id}_extracted`, output_path)
-                // }
+                const extracted =   await fs.promises.opendir(extracted_file)
+                for await (const dirent of extracted) {
+                    console.log(`Generating stub for ${dirent.name}`)
+                    await extractStubs(dirent.name, `${unique_id}_extracted`, output_path)
+                }
             }
         }
-        // await ZipFile(generatedStubLocation, `${tmp}/nodejs.zip`)
-        // putS3Objects(stub.host, `${stub.path}/nodejs.zip`, `${tmp}/nodejs.zip`)
-        // return { "status": "success" }
+        await ZipFile(generatedStubLocation, `${tmp}/nodejs.zip`)
+        putS3Objects(stub.host, `${stub.path}/nodejs.zip`, `${tmp}/nodejs.zip`)
+        return { "status": "success" }
     }
     catch (err) {
         console.log(`Error encountered :: ${err}`)
