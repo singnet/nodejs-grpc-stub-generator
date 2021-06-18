240790e05b37836cb6a7637b6788ec83a9807872
DhivakharVenkatachalam <dhivakharjv@gmail.com>
2021-06-18 20:50:42 +0530

    Changes to validate proto files without uploading

--- handlers/generateNodeStubs.js
+++ handlers/generateNodeStubs.js
@@ -21,25 +21,32 @@ const temporary_paths = {
 export const handler = async (event) => {
   try {
     console.log(`Generate proto :: ${JSON.stringify(event)}`);
-    var output = getHostAndKeyFromUrl(event["output_s3_path"]);
+    if (event["output_s3_path"].length > 0) {
+      var output = getHostAndKeyFromUrl(event["output_s3_path"]);
+    }
     var input = getHostAndKeyFromUrl(event["input_s3_path"]);
     await getS3FolderContents(input.host, input.path, temporary_paths.base);
     var proto_paths = await getProtoPaths(base);
+    if (proto_paths.length == 0) {
+      throw `Proto files are not found in location ${base}`;
+    }
     for (var i = 0; i < proto_paths.length; i++) {
       await extractStubs(
         proto_paths[i].replace("/tmp", "."),
         temporary_paths.result
       );
     }
-    await ZipFile(
-      path.join(temporary_paths.result, `${unique_id}_download`),
-      temporary_paths.upload
-    );
-    await putS3Objects(
-      output.host,
-      `${output.path}nodejs.zip`,
-      temporary_paths.upload
-    );
+    if (event["output_s3_path"].length > 0) {
+      await ZipFile(
+        path.join(temporary_paths.result, `${unique_id}_download`),
+        temporary_paths.upload
+      );
+      await putS3Objects(
+        output.host,
+        `${output.path}nodejs.zip`,
+        temporary_paths.upload
+      );
+    }
     return { statusCode: 200, status: "success" };
   } catch (err) {
     console.log(`Error encountered :: ${err}`);
