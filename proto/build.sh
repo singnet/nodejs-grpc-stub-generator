0181145c3ff3b2af6c2fc74112ef2484bafad8cc
DhivakharVenkatachalam <dhivakharjv@gmail.com>
2021-07-05 16:14:08 +0530

    Removed shell commets

--- proto/build.sh
+++ proto/build.sh
@@ -1,14 +1,10 @@
-#Generate js files
 $1 \
  --js_out=import_style=commonjs,binary:$2 \
  --grpc_out=$2 \
  --plugin=protoc-gen-grpc=$3 \
   -I $4 $5
 
-# Generate ts files
 $1 \
     --plugin=protoc-gen-ts=$6 \
     --ts_out=$2 \
     -I $4 $5
-
-    
