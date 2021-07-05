41b7114d8416260b663a99a31c815ca8ddfb1691
DhivakharVenkatachalam <dhivakharjv@gmail.com>
2021-07-05 16:11:47 +0530

    PR comments code clean up

--- proto/build.sh
+++ proto/build.sh
@@ -11,21 +11,4 @@ $1 \
     --ts_out=$2 \
     -I $4 $5
 
-# Referance for debugging
-# add any proto sample.proto into proto folder then execute below code
-# # Generate JavaScript code
-# ./node_modules/grpc-tools/bin/protoc \
-#     --js_out=import_style=commonjs,binary:./proto \
-#     --grpc_out=./proto \
-#     --plugin=protoc-gen-grpc=./node_modules/.bin/grpc_tools_node_protoc_plugin \
-#     -I ./proto \
-#     ./proto/sample.proto
-
-# # Generate TypeScript code (d.ts)
-# ./node_modules/grpc-tools/bin/protoc \
-#     --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
-#     --ts_out=./proto \
-#     -I ./proto \
-#     ./proto/sample.proto
-
     
