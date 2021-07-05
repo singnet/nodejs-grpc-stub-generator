0e5db6e504726c90663ac1af72a8090ab5d29dff
DhivakharVenkatachalam <dhivakharjv@gmail.com>
2021-07-05 16:17:00 +0530

    comments added

--- proto/execute.sh
+++ proto/execute.sh
@@ -1,2 +1,13 @@
+# ------------SUMMARY-------------
+# $7 -> cd into temporary location
+
+# $1 -> proto location
+# $2 -> output path
+# $3 -> nodejs plugin
+# $4 -> proto folder path
+# $5 -> proto file path
+# $6 -> js plugin 
+# -------------------------------
+
 cd $7
 sh build.sh $1 $2 $3 $4 $5 $6 $7
